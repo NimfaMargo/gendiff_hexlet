307eb2a3a04d6058825207b9bf972429a15a9484
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-08-07 00:36:57 +0300

    add second test for json

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,14 +1,18 @@
 import path from 'path';
 import fs from 'fs';
-import diff from '../src';
+import diff from '../src/diff';
 
 
 const getFixturePath = fileName => path.join(__dirname, '__fixtures__', fileName);
 
 test('compare json files', () => {
-  const file1 = fs.readFileSync(getFixturePath('before.json'), 'utf-8');
-  const file2 = fs.readFileSync(getFixturePath('after.json'), 'utf-8');
-  const result = diff(file1, file2);
+  const result = diff(getFixturePath('before.json'), getFixturePath('after.json'));
   const expected = fs.readFileSync(getFixturePath('result'), 'utf-8');
+  expect(result).toBe(expected);
+});
+
+test('compare json files atempt 2', () => {
+  const result = diff(getFixturePath('before.json'), getFixturePath('after2.json'));
+  const expected = fs.readFileSync(getFixturePath('result2'), 'utf-8');
   expect(result).toBe(expected);
 });
