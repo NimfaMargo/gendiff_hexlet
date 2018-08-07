940ffc35aea3fac6d62d8089c6fe642f03b47f25
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-08-07 21:15:18 +0300

    move parse logic into a separate file

---
+++ src/parse.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import yamlParser from 'js-yaml';
+import path from 'path';
+
+const getFileFormat = filePath => path.extname(filePath);
+
+const selectPropperFormat = (filePath) => {
+  const selector = {
+    '.json': str => JSON.parse(str),
+    '.yaml': str => yamlParser.safeLoad(str),
+  };
+  return selector[getFileFormat(filePath)];
+};
+
+const parse = (filePath) => {
+  const newStr = fs.readFileSync(`${filePath}`, 'utf-8');
+  return selectPropperFormat(filePath)(newStr);
+};
+
+export default parse;
