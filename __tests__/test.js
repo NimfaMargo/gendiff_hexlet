0b6c9442142fabef65123bce7817817f056d838a
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-08-07 14:59:18 +0300

    rewrite json error test

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import diff from '../src';
+import { parse } from '../src/diff';
 
 const getFixturePath = fileName => path.join(__dirname, '__fixtures__', fileName);
 
@@ -10,8 +11,8 @@ test('compare json files', () => {
   expect(result).toBe(expected);
 });
 
-test('compare json files atempt 2', () => {
-  const result = diff(getFixturePath('before.json'), getFixturePath('after2.json'));
-  const expected = fs.readFileSync(getFixturePath('result2'), 'utf-8');
-  expect(result).toBe(expected);
+test('trows error ', () => {
+  expect(() => {
+    parse(getFixturePath('after2.json'));
+  }).toThrow("File can't be empty");
 });
