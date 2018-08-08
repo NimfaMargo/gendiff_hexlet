6dd62abbbcfa897cf2a03276f1a375fb305b0522
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-08-08 18:13:34 +0300

    delete information about files extentions

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import path from 'path';
 import fs from 'fs';
 import parse from './parse';
-import diff from './diff';
+import findDiffrence from './diff';
 
-const allowedFormats = ['.json', '.yaml', '.ini'];
+export default (filePath1, filePath2) => {
+  const format1 = path.extname(filePath1);
+  const format2 = path.extname(filePath2);
+  const firstFileData = fs.readFileSync(`${filePath1}`, 'utf-8');
+  const secondFileData = fs.readFileSync(`${filePath2}`, 'utf-8');
 
-const validateFilesFormats = (filePath1, filePath2) => {
-  const extension1 = path.extname(filePath1);
-  const extension2 = path.extname(filePath2);
-  const file1 = fs.readFileSync(`${filePath1}`, 'utf-8');
-  const file2 = fs.readFileSync(`${filePath2}`, 'utf-8');
-
-  if (extension1 !== extension2) {
+  if (format1 !== format2) {
     throw new Error('Files formats shood be the same');
-  } else if (!(allowedFormats.includes(extension1)) || !(allowedFormats.includes(extension2))) {
-    throw new Error("This file format isn't supported");
   }
-  const parsed1 = parse(extension1, file1);
-  const parsed2 = parse(extension2, file2);
-  return diff(parsed1, parsed2);
+  const parsed1 = parse(format1, firstFileData);
+  const parsed2 = parse(format2, secondFileData);
+  return findDiffrence(parsed1, parsed2);
 };
-export default validateFilesFormats;
