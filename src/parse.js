e10f96a1afbea165f525575532556fa428450bd8
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-08-08 14:25:41 +0300

    rewrite logic

--- src/parse.js
+++ src/parse.js
@@ -1,20 +1,10 @@
 import ini from 'ini';
-import fs from 'fs';
 import yamlParser from 'js-yaml';
-import path from 'path';
 
-const selectPropperFormat = (filePath) => {
-  const selector = {
-    '.json': str => JSON.parse(str),
-    '.yaml': str => yamlParser.safeLoad(str),
-    '.ini': str => ini.parse(str),
-  };
-  return selector[path.extname(filePath)];
+const selector = {
+  '.json': JSON.parse,
+  '.yaml': yamlParser.safeLoad,
+  '.ini': ini.parse,
 };
 
-const parse = (filePath) => {
-  const newStr = fs.readFileSync(`${filePath}`, 'utf-8');
-  return selectPropperFormat(filePath)(newStr);
-};
-
-export default parse;
+export default (extension, data) => selector[extension](data);
