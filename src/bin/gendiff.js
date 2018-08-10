9212c7924f88d3003b6061132429dee48dff16fd
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-08-10 19:19:15 +0300

    add program format

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,8 +6,8 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'recursive')
   .action((firstConfig, secondConfig) =>
-    console.log(diff(firstConfig, secondConfig)));
+    console.log(diff(firstConfig, secondConfig, program.format)));
 
 program.parse(process.argv);
