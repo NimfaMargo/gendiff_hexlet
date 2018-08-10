d48b2a62189385196613a848b2d6ee1b52dc7929
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-08-10 20:35:43 +0300

    add json render

--- src/renderers/index.js
+++ src/renderers/index.js
@@ -1,9 +1,11 @@
 import recursiveRender from './recursive_render';
 import plainRender from './plain_render';
+import jsonRender from './json_render';
 
 const selector = {
   recursive: recursiveRender,
   plain: plainRender,
+  json: jsonRender,
 };
 
 export default (ast, format) => selector[format](ast);
