232e8d54778ccbacec67772ab4e19ac092b4a521
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-08-10 16:19:01 +0300

    rewrite import

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import path from 'path';
 import fs from 'fs';
 import parse from './parse';
 import findDiffrence from './ast';
-import render from './renderers/..';
+import render from './renderers';
 
-export default (filePath1, filePath2) => {
+export default (filePath1, filePath2, format = 'recursive') => {
   const format1 = path.extname(filePath1);
   const format2 = path.extname(filePath2);
   const firstFileData = fs.readFileSync(`${filePath1}`, 'utf-8');
@@ -15,5 +15,5 @@ export default (filePath1, filePath2) => {
   }
   const parsed1 = parse(format1, firstFileData);
   const parsed2 = parse(format2, secondFileData);
-  return render(findDiffrence(parsed1, parsed2));
+  return render(findDiffrence(parsed1, parsed2), format);
 };
