c80057cd013023b07681fc47f6f9999fb0b9e29c
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-08-10 16:07:17 +0300

    add comment

--- src/renderers/index.js
+++ src/renderers/index.js
@@ -1,9 +1,9 @@
 import recursiveRender from './recursive_render';
-import plainRender from './plain_render';
+// import plainRender from './plain_render';
 
 const selector = {
   recursive: recursiveRender,
-  plain: plainRender,
+  // plain: plainRender,
 };
 
 export default format => selector[format];
